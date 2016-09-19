875d9834cd1e433f95d9075434e8ea99c4675066
lzmkhan <lzmkhan@yahoo.com>
2016-09-19 16:11:46 +0530

    Naukri.com Resume Auto upload script
    Automatically logs in the naukri portal, and uploads the resume.
    The email, password and resume path should be given in
    Naukri_Automation.html. Needs Selenium Server.jar.  Edit the firefox
    location to the actual installed location in start.bat batch file.

---
+++ Naukri_Resume_Auto_Upload/start.bat
@@ -0,0 +1,2 @@
+echo off
+java -jar selenium-server.jar -htmlSuite "*firefox C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "https://www.naukri.com/" "Naukri_Automation_Suite.html" "results.html"  
